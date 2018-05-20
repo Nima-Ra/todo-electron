a14c0deaa5d3c88bf3937d9968ea23f78b9f04a7
Nima Rasooli <nimarasooli1@gmail.com>
2018-05-20 15:24:09 +0430

    Add files via upload

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -14,6 +14,45 @@ Array.prototype.remove = function() {
     return this;
 };
 
+
+function addWindow(){
+  ipcRenderer.send('todo:window');
+}
+
+function removeTodo(i){
+  let data = fs.readFileSync('Todo.txt').toString().split(" | ");
+  data.remove(data[i]);
+  let out = '';
+  for (var j = 0 ; j < data.length ; j++){
+    if (j == 0){
+      out += data[j]
+    }
+    else{
+      out += " | " + data[j]
+    }
+  }
+
+  // fs.writeFileSync("Todo.txt", out);
+  fs.writeFile("Todo.txt", out, (err) => {
+    if (err) console.log(err);
+  });
+
+  document.getElementById(i).remove();
+  if (ul.children.length == 0){
+    ul.className = '';
+  }
+}
+
+function i_make(i){
+  i += 1
+  if (document.getElementById(i) == null){
+    return i;
+  }
+  else{
+    i_make(i);
+  }
+}
+
 function add_todo_file(todo){
 
   fs.appendFile('Todo.txt',todo + " | ", function(err){
@@ -67,16 +106,6 @@ if (todo_data.length > 1){
   }
 }
 
-
-function i_make(i){
-  i += 1
-  if (document.getElementById(i) == null){
-    return i;
-  }
-  else{
-    i_make(i);
-  }
-}
 // Catch add todo
 ipcRenderer.on('todo:add', function(e, todo){
   add_todo_file(todo);
@@ -89,41 +118,3 @@ ipcRenderer.on('todo:clear', function(e){
   ul.className = '';
   fs.writeFileSync("Todo.txt", '');
 });
-function removeTodo(i){
-  let data = fs.readFileSync('Todo.txt').toString().split(" | ");
-  data.remove(data[i]);
-  let out = '';
-  for (var j = 0 ; j < data.length ; j++){
-    if (j == 0){
-      out += data[j]
-    }
-    else{
-      out += " | " + data[j]
-    }
-  }
-
-  // fs.writeFileSync("Todo.txt", out);
-  fs.writeFile("Todo.txt", out, (err) => {
-    if (err) console.log(err);
-  });
-
-  document.getElementById(i).remove();
-  if (ul.children.length == 0){
-    ul.className = '';
-  }
-}
-
-function addWindow(){
-  ipcRenderer.send('todo:window');
-}
-var button = document.createElement("a");
-button.className = "btn-floating btn-large waves-effect waves-light red";
-button.id = "add-todo-button";
-button.addEventListener("click", addWindow);
-var i = document.createElement('i');
-i.className = "fa fa-plus";
-button.appendChild(i);
-button.style.bottom = "2%";
-button.style.right = "2%";
-button.style.position = 'absolute';
-document.getElementById("body").appendChild(button);
